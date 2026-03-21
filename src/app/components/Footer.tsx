41e9d12456c754ae6ed98b3895173a3aef221f6d
igorhariton <igor.hariton@isa.utm.md>
2026-03-21 13:24:42 +0200

    use Link from react-router-dom in Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Instagram, Twitter, Facebook, Youtube, Mail, Phone, MapPin } from 'lucide-react';
 import { useApp } from '../context/AppContext';
 
