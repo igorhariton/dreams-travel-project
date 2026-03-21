b9f45182d7ed4edb16deb8e5867fe589bc1689d0
igorhariton <igor.hariton@isa.utm.md>
2026-03-21 13:25:10 +0200

    use Link from react-router-dom in Footer

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, useLocation } from 'react-router';
+import { Outlet, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'motion/react';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
