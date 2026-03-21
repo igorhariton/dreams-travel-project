c16404664cbfaa768f9a5921cabfd68226f4db3a
igorhariton <igor.hariton@isa.utm.md>
2026-03-21 13:23:23 +0200

    correct router import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouterProvider } from 'react-router';
+import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { AppProvider } from './context/AppContext';
 
