afb4b19f5669d6634783010068c10fb14c556afc
Igor Hariton <igorhariton8@gmail.com>
2026-04-25 17:12:49 +0300

    Add user domain entities

---
+++ backend/TravelBackend/TravelBackend.Domain/Entities/User/UserData.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelBackend.Domain.Entities.User
+{
+    public class UserData
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [StringLength(30)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(30, MinimumLength = 4)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(60)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(48, MinimumLength = 8)]
+        public string Password { get; set; } = string.Empty;
+
+        [StringLength(12)]
+        public string Phone { get; set; } = string.Empty;
+
+        public UserRole Role { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RegisteredOn { get; set; }
+    }
+}
---
+++ backend/TravelBackend/TravelBackend.Domain/Entities/User/UserRole.cs
@@ -0,0 +1,9 @@
+namespace TravelBackend.Domain.Entities.User
+{
+    public enum UserRole
+    {
+        User = 1,
+        Host = 20,
+        Admin = 30
+    }
+}
