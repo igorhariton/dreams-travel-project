1a057a0c415288e9467aa6f502257e871397068a
Igor Hariton <igorhariton8@gmail.com>
2026-04-25 17:13:43 +0300

    Add action response model

---
+++ backend/TravelBackend/TravelBackend.Domain/Models/Responces/ActionResponce.cs
@@ -0,0 +1,8 @@
+namespace TravelBackend.Domain.Models.Responces
+{
+    public class ActionResponce
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+    }
+}
