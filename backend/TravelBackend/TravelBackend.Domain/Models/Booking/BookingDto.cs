746d66c9b27c5de0073f314a918d305d188b1257
Igor Hariton <igorhariton8@gmail.com>
2026-04-25 17:13:44 +0300

    Add booking model

---
+++ backend/TravelBackend/TravelBackend.Domain/Models/Booking/BookingDto.cs
@@ -0,0 +1,17 @@
+using TravelBackend.Domain.Entities.Booking;
+
+namespace TravelBackend.Domain.Models.Booking
+{
+    public class BookingDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ListingType { get; set; } = string.Empty;
+        public int ListingId { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int Guests { get; set; }
+        public decimal TotalPrice { get; set; }
+        public BookingStatus Status { get; set; }
+    }
+}
