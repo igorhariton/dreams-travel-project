f7cebd13cb4367e70beebf6f2310736ea5f6d0b7
Igor Hariton <igorhariton8@gmail.com>
2026-04-25 17:15:59 +0300

    Configure API startup and Swagger

---
+++ backend/TravelBackend/TravelBackend.Api/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using TravelBackend.DataAccess.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//connection strinf setup
+TravelBackend.DataAccess.DbSession.ConnectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TravelContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
